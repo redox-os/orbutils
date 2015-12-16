6366e0bc35f4021e40f684f746dff216fe5c4ca8
Jeremy Soller <jackpot51@gmail.com>
2015-12-16 12:12:26 -0700

    Replace contents with big text box

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 extern crate orbtk;
 
+use orbtk::*;
+
+fn real_main(){
+    let mut window = Window::new(Rect::new(100, 100, 420, 420), "Editor");
+
+    TextBox::new()
+        .position(0, 0)
+        .size(420, 420)
+        .place(&mut window);
+
+    window.exec();
+}
+
 #[cfg(target_os = "redox")]
 #[no_mangle]
 pub fn main() {
-    orbtk::example();
+    real_main();
 }
 
 #[cfg(not(target_os = "redox"))]
 fn main() {
-    orbtk::example();
+    real_main();
 }
