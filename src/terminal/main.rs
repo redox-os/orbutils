7409989d79615a5ff13e7dd3766f03cf08209e95
Jeremy Soller <jackpot51@gmail.com>
2016-02-09 10:36:20 -0700

    Pipe output and input

--- src/terminal/main.rs
+++ src/terminal/main.rs
@@ -2,10 +2,9 @@ extern crate orbclient;
 
 use orbclient::Color;
 
-use std::ops::DerefMut;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::process::{Command, Stdio};
-use std::str;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 use window::ConsoleWindow;
@@ -15,31 +14,98 @@ mod window;
 fn main() {
     let mut window = ConsoleWindow::new(-1, -1, 576, 400, "Terminal");
 
-    loop {
-        window.print("# ", Color::rgb(255, 255, 255));
-        if let Some(line_original) = window.read() {
-            let line = line_original.trim().to_string();
-            if ! line.is_empty() {
-                match Command::new("sh")
-                        .arg("-c")
-                        .arg(&line)
-                        .stdout(Stdio::piped())
-                        .spawn()
-                {
-                    Ok(process) => {
-                        let mut output = String::new();
-                        match process.stdout.unwrap().read_to_string(&mut output) {
-                            Ok(_) => window.print(&output, Color::rgb(255, 255, 255)),
-                            Err(err) => window.print(&format!("failed to get output from '{}': {}\n", line, err), Color::rgb(255, 0, 0))
+    match Command::new("sh")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+    {
+        Ok(process) => {
+            let output_mutex = Arc::new(Mutex::new(Vec::new()));
+
+            {
+                let mut stdout = process.stdout.unwrap();
+                let stdout_output_mutex = output_mutex.clone();
+                thread::spawn(move || {
+                    'stdout: loop {
+                        let mut buf = [0; 4096];
+                        match stdout.read(&mut buf) {
+                            Ok(0) => break 'stdout,
+                            Ok(count) => match stdout_output_mutex.lock() {
+                                Ok(mut stdout_output) => stdout_output.extend_from_slice(&buf[..count]),
+                                Err(err) => {
+                                    println!("failed to lock stdout output mutex: {}", err);
+                                    break 'stdout;
+                                }
+                            },
+                            Err(err) => {
+                                println!("failed to read stdout: {}", err);
+                                break 'stdout;
+                            }
                         }
+                    }
+                });
+            }
+
+            {
+                let mut stderr = process.stderr.unwrap();
+                let stderr_output_mutex = output_mutex.clone();
+                thread::spawn(move || {
+                    'stderr: loop {
+                        let mut buf = [0; 4096];
+                        match stderr.read(&mut buf) {
+                            Ok(0) => break 'stderr,
+                            Ok(count) => match stderr_output_mutex.lock() {
+                                Ok(mut stderr_output) => stderr_output.extend_from_slice(&buf[..count]),
+                                Err(err) => {
+                                    println!("failed to lock stderr output mutex: {}", err);
+                                    break 'stderr;
+                                }
+                            },
+                            Err(err) => {
+                                println!("failed to read stderr: {}", err);
+                                break 'stderr;
+                            }
+                        }
+                    }
+                });
+            }
+
+            let mut stdin = process.stdin.unwrap();
+            'events: loop {
+                match output_mutex.lock() {
+                    Ok(mut output) => {
+                        let mut string = String::new();
+                        for byte in output.drain(..) {
+                            string.push(byte as char);
+                        }
+                        window.print(&string, Color::rgb(255, 255, 255));
                     },
                     Err(err) => {
-                        window.print(&format!("failed to execute '{}': {}\n", line, err), Color::rgb(255, 0, 0));
+                        println!("failed to lock print output mutex: {}", err);
+                        break 'events;
                     }
                 }
+
+                window.print("# ", Color::rgb(255, 255, 255));
+                if let Some(mut line) = window.read() {
+                    line.push('\n');
+                    match stdin.write(&line.as_bytes()) {
+                        Ok(_) => (),
+                        Err(err) => {
+                            println!("failed to write stdin: {}", err);
+                            break 'events;
+                        }
+                    }
+                } else {
+                    break 'events;
+                }
+
+                thread::sleep_ms(30);
             }
-        } else {
-            break;
+        },
+        Err(err) => {
+            window.print(&format!("failed to execute shell: {}\n", err), Color::rgb(255, 0, 0));
         }
     }
 }
