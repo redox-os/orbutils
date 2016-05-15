4bd9364f3754abdcecad480e9f6150e221273c7b
Jeremy Soller <jackpot51@gmail.com>
2016-05-15 11:33:51 -0600

    Convert to using orbimage

--- src/viewer/main.rs
+++ src/viewer/main.rs
@@ -1,14 +1,14 @@
 #![deny(warnings)]
 
 extern crate orbclient;
-extern crate png;
+extern crate orbimage;
 
 use std::cmp::max;
 
 use std::env;
-use std::slice;
 
-use orbclient::*;
+use orbclient::{Color, Window, EventOption, K_ESC};
+use orbimage::Image;
 
 fn event_loop(window: &mut Box<Window>){
     loop {
@@ -39,48 +39,30 @@ fn main() {
         None => "none:".to_string(),
     };
 
-    if url.ends_with(".bmp") {
-        let bmp = BmpFile::from_path(&url);
-        let mut window = Window::new(-1,
-                                     -1,
-                                     max(320, bmp.width() as u32),
-                                     max(32, bmp.height() as u32),
-                                     &("Viewer (".to_string() + &url + ")"))
-                             .unwrap();
-        window.set(Color::rgb(0, 0, 0));
-        window.image(0, 0, bmp.width() as u32, bmp.height() as u32, &bmp);
-        window.sync();
-        event_loop(&mut window);
-    } else if url.ends_with(".png") {
-        let png = png::load_png(&url).unwrap();
-        let mut window = Window::new(-1,
-                                     -1,
-                                     max(320, png.width),
-                                     max(32, png.height),
-                                     &("Viewer (".to_string() + &url + ")"))
-                             .unwrap();
-        window.set(Color::rgb(0, 0, 0));
-        match png.pixels {
-            png::PixelsByColorType::K8(_data) => error_msg(&mut window, "Does not support K8"),
-            png::PixelsByColorType::KA8(_data) => error_msg(&mut window, "Does not support KA8"),
-            png::PixelsByColorType::RGB8(_data) => error_msg(&mut window, "Does not support RGB8"),
-            png::PixelsByColorType::RGBA8(data) => {
-                let data_colors = unsafe { slice::from_raw_parts(data.as_ptr() as *const Color, data.len()/4) };
-                window.image(0, 0, png.width, png.height, data_colors);
-            }
+    match Image::from_path(&url) {
+        Ok(image) => {
+            let mut window = Window::new(-1,
+                                         -1,
+                                         max(320, image.width()),
+                                         max(32, image.height()),
+                                         &("Viewer (".to_string() + &url + ")"))
+                                 .unwrap();
+            window.set(Color::rgb(0, 0, 0));
+            window.image(0, 0, image.width(), image.height(), &image.data());
+            window.sync();
+            event_loop(&mut window);
+        },
+        Err(err) => {
+            let mut window = Window::new(-1,
+                                         -1,
+                                         320,
+                                         32,
+                                         &("Viewer (".to_string() + &url + ")"))
+                                 .unwrap();
+            window.set(Color::rgb(0, 0, 0));
+            error_msg(&mut window, &format!("{}", err));
+            window.sync();
+            event_loop(&mut window);
         }
-        window.sync();
-        event_loop(&mut window);
-    } else {
-        let mut window = Window::new(-1,
-                                     -1,
-                                     320,
-                                     32,
-                                     &("Viewer (".to_string() + &url + ")"))
-                             .unwrap();
-        window.set(Color::rgb(0, 0, 0));
-        error_msg(&mut window, "Unknown image type");
-        window.sync();
-        event_loop(&mut window);
     }
 }
