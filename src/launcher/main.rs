0ef5e8b60162432fa5c3ac3c6674e125566aadd3
Jeremy Soller <jeremy@system76.com>
2020-12-21 21:33:56 -0700

    Fix detection of selected icon

--- src/launcher/main.rs
+++ src/launcher/main.rs
@@ -335,7 +335,7 @@ fn bar_main() {
 
                 {
                     let mut x = 0;
-                    let y = 0;
+                    let y = self.offset;
                     let mut i = 0;
 
                     {
