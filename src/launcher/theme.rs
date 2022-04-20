9bb13b488498cba4634b68af702a7d5b9f6d9914
Jeremy Soller <jackpot51@gmail.com>
2022-04-19 20:17:08 -0600

    Adjust theme to match orbital

--- src/launcher/theme.rs
+++ src/launcher/theme.rs
@@ -1,6 +1,6 @@
 use orbclient::Color;
 
 pub const BAR_COLOR: Color = Color::rgba(47, 52, 63, 224);
-pub const BAR_HIGHLIGHT_COLOR: Color = Color::rgb(80, 86, 102);
+pub const BAR_HIGHLIGHT_COLOR: Color = Color::rgba(80, 86, 102, 224);
 pub const TEXT_COLOR: Color = Color::rgb(204, 210, 224);
 pub const TEXT_HIGHLIGHT_COLOR: Color = Color::rgb(235, 241, 255);
