923f29591f73384c61f4ca37692ce7f8a94a5315
kivimango <balazsviktor23@gmail.com>
2023-05-07 21:57:07 +0200

    Wire slint ui and orblogin logic together

--- orblogin/src/main.rs
+++ orblogin/src/main.rs
@@ -11,9 +11,9 @@ mod generated_code {
 }
 
 pub use generated_code::*;
-use slint::SharedString;
-use std::{env, str};
+use slint::{invoke_from_event_loop, SharedString};
 use std::process::Command;
+use std::{env, str};
 
 /*use orbclient::{Color, EventOption, Renderer, Window, WindowFlag};
 use orbfont::Font;
@@ -82,26 +82,7 @@ fn find_scale(image: &Image, mode: BackgroundMode, display_width: u32, display_h
     }
 }*/
 
-fn normal_usernames() -> Vec<String> {
-    let users = match AllUsers::authenticator(Config::default()) {
-        Ok(ok) => ok,
-        Err(_) => {
-            return Vec::new();
-        }
-    };
-
-    let mut usernames = Vec::new();
-    for user in users.iter() {
-        if user.uid >= 1000 {
-            usernames.push(user.user.clone());
-        }
-    }
-    usernames.sort();
-    usernames
-}
-
-fn login_command(username: &str, pass: &str, launcher_cmd: &str, launcher_args: &[String]) -> Option<Command> {
-
+fn login_command(username: &str, launcher_cmd: &str, launcher_args: &[String]) -> Option<Command> {
     let sys_users = match AllUsers::authenticator(Config::default()) {
         Ok(users) => users,
         // Not maybe the best thing to do...
@@ -110,18 +91,15 @@ fn login_command(username: &str, pass: &str, launcher_cmd: &str, launcher_args: 
         }
     };
 
-    match sys_users.get_by_name(&username) {
-        Some(user) => if user.verify_passwd(&pass) {
-            let mut command = user.login_cmd(&launcher_cmd);
+    match sys_users.get_by_name(username) {
+        Some(user) => {
+            let mut command = user.login_cmd(launcher_cmd);
             for arg in launcher_args.iter() {
-                command.arg(&arg);
+                command.arg(arg);
             }
-
             Some(command)
-        } else {
-            None
-        },
-        None => None
+        }
+        None => None,
     }
 }
 
@@ -368,27 +346,74 @@ fn login_command(username: &str, pass: &str, launcher_cmd: &str, launcher_args: 
     }
 }*/
 
+fn authenticate(username: SharedString, password: SharedString) -> bool {
+    let username = String::from(username);
+    let password = String::from(password);
+
+    let sys_users = match AllUsers::authenticator(Config::default()) {
+        Ok(users) => users,
+        // Not maybe the best thing to do...
+        Err(_) => {
+            return false;
+        }
+    };
+
+    match sys_users.get_by_name(&username) {
+        Some(user) => {
+            if user.verify_passwd(password) {
+                return true;
+            }
+            false
+        }
+        None => false,
+    }
+}
+
 fn main() {
     let mut args = env::args().skip(1);
 
-    let launcher_cmd = args.next().expect("orblogin: no window manager command");
+    let launcher_cmd = args
+        .next()
+        .expect("orblogin: no window manager command provided!");
     let launcher_args: Vec<String> = args.collect();
 
-    /*loop {
-        if let Some(mut command) = login_window(&launcher_cmd, &launcher_args) {
-            match command.spawn() {
-                Ok(mut child) => match child.wait() {
-                    Ok(_) => (),
-                    Err(err) => {
-                        println!("orblogin: failed to wait for '{}': {}", launcher_cmd, err);
-                    }
-                },
-                Err(err) => {
-                    println!("orblogin: failed to execute '{}': {}", launcher_cmd, err);
+    let login_window = LoginWindow::new();
+    login_window.on_authenticate(authenticate);
+
+    let login_window_weak = login_window.as_weak();
+    login_window.on_exec_login_cmd(move || {
+        let login_window_weak_copy = login_window_weak.clone();
+        let launcher_cmd = launcher_cmd.clone();
+        let launcher_args = launcher_args.clone();
+
+        invoke_from_event_loop(move || {
+            let login_window = login_window_weak_copy.unwrap();
+            let is_authenticated: bool = !login_window.get_login_failed();
+
+            if is_authenticated {
+                // dequeue this window from the rendering loop
+                login_window.hide();
+
+                let username = String::from(login_window.get_username_input());
+                match login_command(&username, &launcher_cmd, &launcher_args) {
+                    Some(mut login_cmd) => match login_cmd.spawn() {
+                        Ok(mut child) => match child.wait() {
+                            Ok(_) => (),
+                            Err(error) => {
+                                eprintln!("orblogin: failed to wait for '{launcher_cmd}' : {error}")
+                            }
+                        },
+                        Err(error) => {
+                            eprintln!("orblogin: failed to execute '{launcher_cmd}': {error}")
+                        }
+                    },
+                    // there is a valid user without login command
+                    None => println!("login completed without a command!"),
                 }
             }
-        }
-    }*/
-    let login_window = LoginWindow::new().expect("Failed to initialize LoginWindow!");
-    login_window.run().expect("Failed to start LoginWindow!");
+        })
+        .expect("orblogin: upgrade_in_event_loop() returned Err!");
+    });
+
+    login_window.run();
 }
