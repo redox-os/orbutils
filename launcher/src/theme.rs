cb21773b5fdbb47bd4939f84d6234f564a5744e4
Jeremy Soller <jackpot51@gmail.com>
2024-06-06 09:26:16 -0600

    Adjust launcher colors

--- launcher/src/theme.rs
+++ launcher/src/theme.rs
@@ -1,6 +1,6 @@
 use orbclient::Color;
 
-pub const BAR_COLOR: Color = Color::rgba(47, 52, 63, 224);
-pub const BAR_HIGHLIGHT_COLOR: Color = Color::rgba(80, 86, 102, 224);
-pub const TEXT_COLOR: Color = Color::rgb(204, 210, 224);
-pub const TEXT_HIGHLIGHT_COLOR: Color = Color::rgb(235, 241, 255);
+pub const BAR_COLOR: Color = Color::rgba(0x1B, 0x1B, 0x1B, 224);
+pub const BAR_HIGHLIGHT_COLOR: Color = Color::rgba(0x36, 0x36, 0x36, 224);
+pub const TEXT_COLOR: Color = Color::rgb(0xE7, 0xE7, 0xE7);
+pub const TEXT_HIGHLIGHT_COLOR: Color = Color::rgb(0xE7, 0xE7, 0xE7);
